fb599ab5c8164c8823e12f3a4818152c2c4b7a4b
Your Name <almtyrybtal1@gmail.com>
2024-03-21 21:01:04 +0300

    waslah.sa

--- home/home.js
+++ home/home.js
@@ -33,3 +33,17 @@ window.addEventListener("load", function () {
     heroBox2.appendChild(heroHeading2);
     heroBox2.appendChild(heroDesc2);
 })
+
+let main_click = document.getElementById('main-click');
+let main_menu = document.getElementById('main-menu');
+let main = document.querySelector('main');
+let ul = document.getElementById('main-menu-ul')
+main.onclick = () => {
+    main_menu.classList.remove('show-menu');
+    ul.style.display = 'none';
+};
+main_click.onclick = () => {
+    main_menu.classList.add('show-menu');
+    ul.style.display = 'flex';
+};
+
